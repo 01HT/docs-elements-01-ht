80a5737e685a0aae6d5ce376eae45c627259f12a
maksimyurkov <yurkov.maksim.sergeevich@gmail.com>
2018-09-09 19:47:20 +0300

    move to VuePress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,79 @@
+module.exports = {
+  dest: "vuepress",
+  title: "ElementsDocs",
+  description: "Руководство пользователя Elements",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: `http://res.cloudinary.com/cdn-01ht/image/upload/v1531730403/logos/01ht/elements/logo.png`
+      }
+    ],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+    [
+      "meta",
+      { name: "apple-mobile-web-app-status-bar-style", content: "black" }
+    ],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        href: `http://res.cloudinary.com/cdn-01ht/image/upload/c_scale,w_152/v1531730403/logos/01ht/elements/logo.png`
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href:
+          "http://res.cloudinary.com/cdn-01ht/image/upload/c_scale,w_152/v1531730403/logos/01ht/elements/logo.svg",
+        color: "#3eaf7c"
+      }
+    ],
+    [
+      "meta",
+      {
+        name: "msapplication-TileImage",
+        content:
+          "http://res.cloudinary.com/cdn-01ht/image/upload/c_scale,w_144/v1531730403/logos/01ht/elements/logo.png"
+      }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#000000" }]
+  ],
+  serviceWorker: true,
+  // theme: "vue",
+  themeConfig: {
+    repo: "01ht/ht-docs-elements",
+    editLinks: true,
+    docsDir: "docs",
+    nav: [
+      { text: "Руководство", link: "/guide/" },
+      { text: "Для разработчиков", link: "/developers/" }
+      // { text: "Elements", link: "https://elements.01.ht" }
+    ],
+    editLinkText: "Редактировать страницу на GitHub",
+    lastUpdated: "Последнее обновление",
+    serviceWorker: {
+      updatePopup: {
+        message: "Доступен новый контент обновите страницу",
+        buttonText: "Обновите страницу"
+      }
+    },
+    sidebar: {
+      "/guide/": genSidebarConfig("Руководство"),
+      "/developers/": genSidebarConfig("Для разработчиков")
+    }
+  }
+};
+
+function genSidebarConfig(title) {
+  switch (title) {
+    case "Руководство":
+      return [{ title, collapsable: false, children: ["", "signin", "purchase"] }];
+    case "Для разработчиков":
+      return [{ title, collapsable: false, children: ["", "getting-started", "payments", "verification"] }];
+  }
+}
---
+++ docs/guide/README.md
@@ -0,0 +1,23 @@
+# Введение
+
+[Link](https://yandex.ru)
+
+![Image from alias](https://res.cloudinary.com/cdn-01ht/image/upload/v1535207284/users/O56irHsSXsaEssaAyW06FYBAnAp1.png)
+
+## One
+
+## Two
+
+## Three
+
+``` js
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': 'path/to/some/dir'
+      }
+    }
+  }
+}
+```
